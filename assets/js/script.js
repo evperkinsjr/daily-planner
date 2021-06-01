6eb5923f19ccff6e57360b85ab0b43a6d54e6fa5
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-06-01 17:09:42 -0400

    pseudocoding the js file

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Display current date on page
+
+// Listen for save button clicks
+
+// Current number of hours
+
+// Check if block is past, present or future
+
+
+// Load data from localStorage
