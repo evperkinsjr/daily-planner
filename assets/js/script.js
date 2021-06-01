bbe514ff846438c7f204293b6d125f702311522b
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-06-01 17:25:49 -0400

    added on click listener for the save buttons on each timeblock to save input to local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@
 $('#currentDay').text(moment().format('dddd, MMMM Do'));
 
 // Listen for save button clicks
+$('.saveBtn').on('click', function () {
+    var userInput = $(this).siblings('.description').val();
+    var hour = $(this).parent().attr('.id');
+
+    localStorage.setItem(hour, userInput);
+});
 
 // Current number of hours
 
